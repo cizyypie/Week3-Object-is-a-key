ce4e466c31dba0626dd72dc9eaee17176c099a14
Joanita Timbin Panggalo <joanitatimbinp@gmail.com>
2025-03-04 21:40:09 +0700

    update p3 soal1

--- quiz/part3/soal1.js
+++ quiz/part3/soal1.js
@@ -9,14 +9,12 @@ function changeMe(arr) {
             firstName: arr[i][0],
             lastName: arr[i][1],
             gender: arr[i][2],
-            age: 2025-arr[i][3],
+            age: arr[i][3] ? 2025 - arr[i][3] : "Invalid Birth Year",
         }
         details.push(myObj)
     };
-    console.log (details)
+    console.log(details)
 }
-
-// maap bang ga dpt2 cara if else dalam object wkwkwkkw
 
 // TEST CASES
 changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
